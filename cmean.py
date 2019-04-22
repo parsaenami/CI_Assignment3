4afa74ad2da9bf1161e4641331e2f1df2144f3cf
Parsa Enami <parsaenami@aut.ac.ir>
2019-04-22 15:59:43 +0430

    function added

--- cmean.py
+++ cmean.py
@@ -17,7 +17,6 @@ def random_data_generator(dimension, limit=100, randomness=0.99):
     return data_temp
 
 
-# TODO: change it like previous function
 def random_membership_generator(data_in, dimension):
     membership = np.random.dirichlet(np.ones(len(data_in)))
     membership_temp = []
@@ -30,39 +29,65 @@ def random_membership_generator(data_in, dimension):
 
     return membership_temp
 
-# [
-# [62, 5],
-# [6, 1],
-# [64, 11],
-# [64, 44],
-# [21, 64]
-# ]
 
 def center_initialize(n_cluster, dimension, limit=100):
     cell, centers = [], []
     for n in range(n_cluster):
         for d in range(dimension):
             cell.append(r.randrange(limit))
-        centers.append(cell)
+        centers.append(cell.copy())
+        cell.clear()
 
     return centers
 
 
+def update_centers():
+    pass
+
+
+def update_membership():
+    pass
+
+
+def distance(x, c):
+    dim = x.__len__()
+    sum = 0
+    for d in range(dim):
+        sum += (x[d] - c[d]) ** 2
+
+    return sum ** (0.5)
+
+
+
 random_data = random_data_generator(2)
 membership_matrix = random_membership_generator(random_data, 2)
+cluster_centers = center_initialize(4, 2)
 data = np.array(random_data)
+centers = np.array(cluster_centers)
+
 
 print('random data = ', random_data)
 print('membership = ', membership_matrix)
+print('centers = ', cluster_centers)
+
+
+
+
+
+
+
+
+
 
 ##################### TEST AREA #####################
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(data)
-labels = kmeans.labels_
-
-# print(labels)
-
-plt.scatter(x=data[:, 0], y=data[:, 1], s=30, c=labels)
-plt.grid(True)
-plt.show()
+# kmeans = KMeans(n_clusters=4)
+# kmeans.fit(data)
+# labels = kmeans.labels_
+#
+# # print(labels)
+#
+# plt.scatter(x=data[:, 0], y=data[:, 1], s=20, c=labels)
+# plt.scatter(x=centers[:, 0], y=centers[:, 1], s=30, marker='D', c='black')
+# plt.grid(True)
+# plt.show()
 #####################################################
